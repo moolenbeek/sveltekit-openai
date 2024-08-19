8fad065c7dcdb715a9bcf4ee21de1a7be3afd9c5
moolenbeek <emoolenbeek@gmail.com>
2024-08-19 07:54:49 -0400

    display openai message

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,5 @@
 import { OPENAI_API_KEY } from "$env/static/private"
+import type { PageServerLoad } from './$types';
 
 import { OpenAI } from 'openai';
 const openai = new OpenAI({
@@ -6,15 +7,25 @@ const openai = new OpenAI({
 });
 export default openai;
 
+const lat = "45.424721";
+const lon = "-75.695000";
+
+
 const completion = await openai.chat.completions.create({
   model: "gpt-4o-mini",
   messages: [
       { role: "system", content: "You are a helpful assistant." },
       {
           role: "user",
-          content: "Write a haiku about recursion in programming.",
+          content: `In one simple sentence what should I wear today? latitude = ${lat} longitude = ${lon}`,
       },
   ],
 });
 
 console.log(completion.choices[0].message);
+
+export const load: PageServerLoad = async () => {
+	return {
+		serverMessage: completion.choices[0].message,
+	};
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+	const message = data.serverMessage.content;
+</script>
+
 <h1 class="flex items-center justify-center">Open AI</h1>
+
+<p class="p-4 flex items-center justify-center">{message}</p>
