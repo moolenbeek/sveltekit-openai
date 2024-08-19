d1582fae2f2cf4bd87bae7951e490c91af71ef08
moolenbeek <emoolenbeek@gmail.com>
2024-08-19 07:33:16 -0400

    uncomment page server

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,20 +1,20 @@
-// import { OPENAI_API_KEY } from "$env/static/private"
+import { OPENAI_API_KEY } from "$env/static/private"
 
-// import { OpenAI } from 'openai';
-// const openai = new OpenAI({
-//   apiKey: OPENAI_API_KEY
-// });
-// export default openai;
+import { OpenAI } from 'openai';
+const openai = new OpenAI({
+  apiKey: OPENAI_API_KEY
+});
+export default openai;
 
-// const completion = await openai.chat.completions.create({
-//   model: "gpt-4o-mini",
-//   messages: [
-//       { role: "system", content: "You are a helpful assistant." },
-//       {
-//           role: "user",
-//           content: "Write a haiku about recursion in programming.",
-//       },
-//   ],
-// });
+const completion = await openai.chat.completions.create({
+  model: "gpt-4o-mini",
+  messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      {
+          role: "user",
+          content: "Write a haiku about recursion in programming.",
+      },
+  ],
+});
 
-// console.log(completion.choices[0].message);
+console.log(completion.choices[0].message);
