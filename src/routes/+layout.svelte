73f4675bcf7504d6ac098a3452b8f7e2ef3fd267
moolenbeek <emoolenbeek@gmail.com>
2024-08-18 20:15:28 -0400

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="p-4">
+	<LightSwitch />
+</div>
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="flex items-center justify-center">Open AI</h1>
